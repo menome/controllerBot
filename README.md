863f4f497849f024400803610dbc00f2caf24983
mbe <mbe@menome.com>
2020-05-12 21:42:02 -0600

    implemented keyword parameter for todaysDate

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ControllerBot
 
 Bot that controls other bots.
+
+You can use @todayShortDate as a paramater value and the thing that does stuff will replace it with todays short date. ISO. As per Node. Whatever ISO that is. *shrugs*
--- app/dispatcher.js
+++ app/dispatcher.js
@@ -11,18 +11,27 @@ module.exports = {
   dispatch
 }
 
-function dispatch({id, path, method, params, body}) {
-  return registry.get({forceRefresh: false}).then((reg) => {
-    var thisBot = reg.find(x=>x.id === id);
-    if(!thisBot) throw new Error("No bot with this ID found");
-    var thisOp = thisBot.operations.find((x)=>{return (x.method === method && x.path === path) });
-    if(!thisOp) throw new Error("Operation not found");
+function dispatch({ id, path, method, params, body }) {
+  return registry.get({ forceRefresh: false }).then((reg) => {
+    var thisBot = reg.find(x => x.id === id);
+    if (!thisBot) throw new Error("No bot with this ID found");
+    var thisOp = thisBot.operations.find((x) => { return (x.method === method && x.path === path) });
+    if (!thisOp) throw new Error("Operation not found");
+
+    if (params) {      
+      params = JSON.parse(JSON.stringify(params).replace(/@\{(\w+)\}/g, function (match, group) {
+        if (group === 'todayShortDate') {
+          var datetime = new Date();
+          return datetime.toISOString().slice(0, 10);
+        }
+      }))      
+    }
 
     var options = {
       json: true,
       method: thisOp.method,
       qs: params,
-      uri: "http://"+thisBot.address + thisOp.path,
+      uri: "http://" + thisBot.address + thisOp.path,
       body: body
     }
 
